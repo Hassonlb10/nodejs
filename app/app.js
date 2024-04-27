d7afa29c2efa3f183639ea63db56ff3ce5f5c8ed
b03-hassan-hk1908756 <hk1908756@qu.edu.qa>
2024-04-27 14:26:18 +0300

    updated

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs'); // For reading files
+//const fs = require('fs'); // For reading files
 //const { detectHateSpeech } = require('./trained_model'); // Replace with the appropriate module and function
 
 const app = express();
